66a2e6c1c68e562523dc78a3cdde370ae5560862
Gustaf Pahlevi <gustafpahlevi@gmail.com>
2018-04-18 13:24:22 +0700

    Update Redux and Navigation

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+import Home from './src/screens/Home';
+import store from './src/redux/store';
+
+
+const RootStack = StackNavigator({
+  Home: {
+    screen: Home
+  }
+}, {
+  initialRouteName: 'Home'
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store= { store } >
+        <RootStack />
+      </Provider>
     );
   }
 }
---
+++ src/redux/main_redux/reducers.js
@@ -0,0 +1,15 @@
+const initialState = {
+    //Content initial state
+}
+
+const reducer = (state = initialState, action) => {
+    //Switch case conditions
+    // switch (key) {
+    //     case value:
+    //     default:
+    //         return state
+    // }
+    return state
+}
+
+export default reducer
