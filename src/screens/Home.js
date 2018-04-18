748e0d07bc919426a91e52bb847fe3a02a64ed80
Reza Pramudhika <rezapramudhika@gmail.com>
2018-04-18 16:45:24 +0700

    Upload cv from camera roll

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import {  View, Text, } from 'react-native';
+import UploadCVButton from '../components/UploadCVButton';
 
 export default class Home extends Component {
   render() {
     return (
       <View>
         <Text> Home </Text>
+        <UploadCVButton />
       </View>
     );
   }
