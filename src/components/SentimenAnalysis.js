62569d8471f5c186a3ef9996b6618fbc4081ff50
srohimah <sitirohimach@gmail.com>
2018-04-21 14:40:46 +0700

    pie chart sentiment analys

--- src/components/SentimenAnalysis.js
+++ src/components/SentimenAnalysis.js
@@ -1,30 +1,49 @@
 import React, { Component } from 'react';
-import { Text, Body, Content, Left,Separator, ListItem } from 'native-base'
-import { View, StyleSheet } from 'react-native'
-import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
+import { Body, Content, Left,Separator, ListItem } from 'native-base'
+import { AppRegistry, StyleSheet, ScrollView , StatusBar, Text, View } from 'react-native';
+import PieChart from 'react-native-pie-chart';
 
 export default class SentimentAnalys extends Component {
   render() {
+    const chart_wh = 250
+    const sliceColor = ['#F44336','#2196F3','#FFEB3B']
+    const series = [0.33, 0.33, 0.33]
     return (
       <Content>
           <Separator bordered>
             <Text style={{fontSize:18, color:"#3498db"}}>Sentiment Analys Result</Text>
           </Separator>
-          <ListItem icon>
             <Left>
               {/* <FontAwesome name="birthday-cake" /> */}
             </Left>
             <Body>
-              <Text>disini grafik</Text>
+            <ScrollView style={{flex: 1}}>
+              <View style={styles.container}>
+                <StatusBar
+                  hidden={true}
+                />
+                <Text style={styles.title}>Sentiment Analysis Result</Text>
+                <PieChart
+                  chart_wh={chart_wh}
+                  series={series}
+                  sliceColor={sliceColor}
+                />
+              </View>
+            </ScrollView>
             </Body>
-          </ListItem>
        </Content>
     );
   }
 } 
 
 const styles = StyleSheet.create({
-  space: {
-    paddingTop: 20,
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 18,
+    margin: 10
   }
-})
+});
+ 
