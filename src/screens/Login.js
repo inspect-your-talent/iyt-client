d32a2f0fa247d4d98b8462e5816fcb2856963176
Gustaf Pahlevi <gustafpahlevi@gmail.com>
2018-04-24 13:43:14 +0700

    Login UI

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
-import {  View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet, Image } from 'react-native';
 import LoginFacebook from '../components/LoginFacebook';
 
 export default class Home extends Component {
 
   static navigationOptions = {
+    title: 'Welcome',
     headerLeft: null
   }
 
   render() {
     return (
-      <View>
-        <LoginFacebook
-        navigation= {this.props.navigation}/>
+      <View style={ styles.container } >
+        <View style={styles.componentFB}>
+          <LoginFacebook
+            navigation={this.props.navigation} />
+        </View>
+        <View style={styles.imageBanneLogin}>
+          <Image  source={require('../assets/img/loginBanner.jpg')}
+            style={{ width: '100%', height: 145 }} />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignContent: 'center',
+    width: '100%',
+    backgroundColor: '#FFF'
+  },
+  imageBanneLogin: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  componentFB: {
+    flex: 1,
+    justifyContent: 'center',
+    marginTop: '30%',
+  }
+})
+
