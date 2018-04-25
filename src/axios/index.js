c18f2ddc5d744d3f881164696a9642cd9c69a170
Reza Pramudhika <rezapramudhika@gmail.com>
2018-04-25 11:38:40 +0700

    Axios endpoint

--- src/axios/index.js
+++ src/axios/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //baseUrl diganti sesuai ip laptop
 const instance = axios.create({
     timeout: 30000,
-    baseURL: 'http://192.168.1.12:3000'
+    baseURL: 'http://35.187.255.216/'
 })
 
 export default instance;
